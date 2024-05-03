5f3fbaf23ab8f5af1bec520b65b1ec20355eb816
babacar35 <150174849+babacar35@users.noreply.github.com>
2024-05-03 23:33:14 +0000

    models posts

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Post as POST
 # Register your models here.
+@admin.register(POST)
+class POSTAdmin(admin.ModelAdmin):
+    list_display = ('title','slug', 'author', 'publish', 'created', 'updated')
+    list_filter = ('author', 'publish', 'created', 'updated')
+    
